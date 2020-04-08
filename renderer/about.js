1b2a5f109a5b7106fda7b33393beced50ba7efc5
John William Davis <jwdavisdeveloper@gmail.com>
2020-04-07 23:44:43 -0700

    testing, clean for now

--- renderer/about.js
+++ renderer/about.js
@@ -4,20 +4,3 @@ const { BrowserWindow } = remote
 const path = require('path')
 
 let testLink = document.getElementById('test')
-
-
-
-testLink.addEventListener('click', (e) => {
-    let secondWindow = new BrowserWindow({ 
-        width: 800, 
-        height: 500,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        parent:mainWindow
-    })
-  
-    secondWindow.on('close', () => { secondWindow = null })
-    secondWindow.loadFile('./html/about.html')
-    secondWindow.show()
-})
