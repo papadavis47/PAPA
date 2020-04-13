8277beea78e233abe50544e3f893ed318e7f647a
John William Davis <jwdavisdeveloper@gmail.com>
2020-04-12 23:02:20 -0700

    small changes to main.html and my about.html

--- renderer/app.js
+++ renderer/app.js
@@ -1,34 +1,47 @@
-const { ipcRenderer } = require('electron');
+// Modules
+const electron = require("electron");
+const { ipcRenderer } = require("electron");
 
-let displayEntry = document.getElementById('modal-2')
+// DOM variables
 
-let wrapper = document.getElementById('wrapper')
+// Used for showing and hiding journal entries
+const displayEntry = document.getElementById("modal-2");
+// Wrapping the main window content
+const wrapper = document.getElementById("wrapper");
+// Holding the h1 that contains the title - this is clickable
+const about = document.getElementById("aboutClickItem");
+// This is the button that creates the journal entry form when clicked
+const entryForm = document.getElementById("journal-entry");
+// This is the div where I am placing journal entries
+const journalHolder = document.getElementById("entry");
+// This is the button to create a new section
+const section = document.getElementById("section");
+// This is the button for creating a scrapbook
+const scrapbook = document.getElementById("scrapbook");
 
+// This will launch the About Page
+about.addEventListener("click", (e) => {
+  ipcRenderer.send("aboutPage");
+});
 
+//-----------------------This is the logic to show the form to input a journal entry ---------
+// ----------------------And prepare the main window to hold the entry ------------------------
+entryForm.addEventListener("click", (e) => {
+  wrapper.style.display = "none";
+  displayEntry.style.display = "flex";
+  ipcRenderer.send("write");
+});
 
+// Add Journal Entry
+ipcRenderer.on("entry:add", (e, thought) => {
+  const paragraph = document.createElement("div");
+  const node = document.createTextNode(thought);
+  paragraph.appendChild(node);
+  journalHolder.appendChild(paragraph);
+});
 
-
-// // Show modal-1 - Journal Entry
-
-// journal.addEventListener("click", (e) => {
-//     wrapper.style.display = "none"
-//     entryForm.style.display = "flex";
-//   });
-
-// Hide modal-1
-// showHome.addEventListener('click', (e) => {
-//   wrapper.style.display = "block"
-//   entryForm.style.display = "none"
-  
-// })
-  
-
-  // Show modal-2
-
-displayEntry.addEventListener("click", (e) => {
-    displayEntry.style.display = "flex";
-  });
-  
-
-
-
+// Launching the spaceWindow
+section.addEventListener("click", (e) => {
+  // Create a section
+  ipcRenderer.send("section:add");
+});
